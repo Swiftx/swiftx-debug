46a0cfea5f62c1d48f3169558e38b2baffd6dff0
胡永强 <tudio@live.cn>
2018-01-19 01:03:57 +0800

    typescript发布

---
+++ lib/Preview/props.d.ts
@@ -0,0 +1,14 @@
+export interface Props {
+    /**
+     * 容器宽度
+     */
+    width?: number;
+    /**
+     * 容器高度
+     */
+    height?: number;
+    /**
+     * 上边距
+     */
+    top?: number;
+}
---
+++ lib/Preview/view.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react" />
+import * as React from "react";
+import { Props } from "./props";
+/**
+ * 视图组件
+ */
+export declare class View extends React.Component<Props> {
+    renderStyle(): {};
+    render(): JSX.Element;
+}
